1c959c6c2d049ccf06585bcd8baf28e2447074ea
Abdalrhman Hamida <96482182+Abdalrhman7mida@users.noreply.github.com>
2023-04-29 16:45:00 +0300

    Dynamic instead of hard coded
    Data is hard coded in JSX, this isn’t realistic, in some situations we need to write it whit our self, but here in the project all of these data (data – time – amount) would be typically dynamic, it means we receive these data from somewhere.

--- src/component/ExpenseItem.tsx
+++ src/component/ExpenseItem.tsx
@@ -1,12 +1,16 @@
 import './ExpenseItem.css'
 
 export default function ExpenseItem() {
+    const expenseDate: Date = new Date(2020, 2, 28);
+    const expenseTitle: string  = "Car insurance";
+    const expenseAmount: number  = 926.45
+
     return (
         <div className='expense-item'>
-            <div>Date</div>
+            <div>{expenseDate.toDateString()}</div>
             <div className='expense-item__description'>
-                <h2>Title</h2>
-                <div className='expense-item__price'>Amount</div>
+                <h2>{expenseTitle}</h2>
+                <div className='expense-item__price'>${expenseAmount}</div>
             </div>
         </div>
     )
