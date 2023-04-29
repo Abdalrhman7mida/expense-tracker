2edfc6f8b44e70e3f694f725befba5d164f0c237
Abdalrhman Hamida <96482182+Abdalrhman7mida@users.noreply.github.com>
2023-04-29 17:35:56 +0300

    Make component reusable
    - Make our component reusable and configurable through using props

--- src/component/ExpenseItem.tsx
+++ src/component/ExpenseItem.tsx
@@ -1,16 +1,13 @@
 import './ExpenseItem.css'
 
-export default function ExpenseItem() {
-    const expenseDate: Date = new Date(2020, 2, 28);
-    const expenseTitle: string  = "Car insurance";
-    const expenseAmount: number  = 926.45
-
+export default function ExpenseItem({title, amount, date}) {
+    
     return (
         <div className='expense-item'>
-            <div>{expenseDate.toDateString()}</div>
+            <div>{date.toDateString()}</div>
             <div className='expense-item__description'>
-                <h2>{expenseTitle}</h2>
-                <div className='expense-item__price'>${expenseAmount}</div>
+                <h2>{title}</h2>
+                <div className='expense-item__price'>${amount}</div>
             </div>
         </div>
     )
