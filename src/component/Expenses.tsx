a06cf89fa429a53c96cf807642780a1a7c7d321c
Abdalrhman Hamida <96482182+Abdalrhman7mida@users.noreply.github.com>
2023-05-02 17:31:49 +0300

    Add `Expenses` componenet - Following "composition", we added this component.

---
+++ src/component/Expenses.tsx
@@ -0,0 +1,23 @@
+import ExpenseItem from './ExpenseItem'
+import './Expenses.css'
+
+
+interface Expense {
+    id: string;
+    title: string;
+    amount: number;
+    date: Date;
+    }
+
+function Expenses({expenses}: {expenses: Expense[]}) {
+    return (
+        <div className='expenses'>
+            <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
+            <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
+            <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
+            <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} />
+        </div>
+    )
+}
+
+export default Expenses;
