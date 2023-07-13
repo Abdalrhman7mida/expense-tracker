982cc3c8c7d50665689651034bbf1381c423e44a
Abdalrhman Hamida <96482182+Abdalrhman7mida@users.noreply.github.com>
2023-07-14 02:30:34 +0300

    feature/expense-filter: filter  and add expenses
    - [x] in `app.tsx` we have added the logic to add new expense for our array
    Notes:
    - some how we have lifted the state up from the expense form to the app.tsx to append the new expense to the array.
    - when you update a state which depends on the previous state snap shot, you have to pass an anonymous function.

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,19 @@ import "./App.css";
 import { Expenses } from "./component/Expenses/Expenses";
 import DUMMY_EXPENSES from "./data/ExpenseItems";
 import NewExpense from "./component/NewExpense/NewExpense";
+import { useState } from "react";
 import { fExpence } from "./types/types";
 
-const addExpenseHandler = (expense: {
-  title: string;
-  amount: number;
-  date: Date;
-  id: string;
-}) => {
-  console.log("in App js");
-  console.log(expense);
-};
 
 const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler = (expense: fExpence) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  }
+
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
